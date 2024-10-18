c44091b7dd71b57f9e23a975087132f905bc94f6
Graklinghunter <nerevarinekhajiit@gmail.com>
2024-10-18 10:10:35 -0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { person } from '@jsonforms/examples';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import React, { useState } from 'react';
+import { JsonForms } from '@jsonforms/react';
+
+
+const schema = person.schema;
+const uischema = person.uischema;
+const initialData = person.data;
+
+function App() {
+  const [data, setData] = useState(initialData);
+  return (
+    <div className='App'>
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, errors }) => setData(data)}
+      />
+    </div>
+  );
+}
+
+export default App;
